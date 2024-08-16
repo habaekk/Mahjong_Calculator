cd67fddb1c477d3a2458bfce3aa34ad36f74812c
habaekk <haback1493@gmail.com>
2024-08-16 20:41:21 +0900

    FEAT: add reach counter state recoil file.

---
+++ src/recoil/reachCountState.js
@@ -0,0 +1,11 @@
+import { selector } from 'recoil';
+import { playersState } from './playerState'; // playersState가 정의된 파일 경로
+
+export const reachCountState = selector({
+  key: 'reachCountState', // 고유한 key
+  get: ({ get }) => {
+    const players = get(playersState); // playersState 상태를 가져옴
+    // tenpai 상태가 true인 플레이어의 수를 계산
+    return players.filter(player => player.reach).length;
+  },
+});
