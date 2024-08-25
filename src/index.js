cec3de79bca99f51c5fd595f53eecd5b21d3c9d3
habaekk <haback1493@gmail.com>
2024-08-25 15:52:36 +0900

    CDCI: add vercel Analytixs

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { RecoilRoot } from 'recoil';
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
       <App />
+      <Analytics />
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
